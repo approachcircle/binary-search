271d6b677f2eb99227842d945b655ae7d49907ec
approachcircle <osc-mcd@protonmail.com>
2024-07-03 16:10:59 +0100

    implement

--- src/main/java/net/approachcircle/demo/BinarySearch.java
+++ src/main/java/net/approachcircle/demo/BinarySearch.java
@@ -1,2 +1,23 @@
-package net.approachcircle.demo;public class BinarySearch {
+package net.approachcircle.demo;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+        public static boolean search(String target, String[] arr) {
+            System.out.println(Arrays.toString(arr));
+            // decided to bitshift instead of divide and floor
+            int midpoint = arr.length >> 1;
+            // exit condition for instances where target is not present
+            if (arr.length == 1) {
+                return target.equals(arr[midpoint]);
+            }
+            if (target.compareTo(arr[midpoint]) == 0) {
+                return true;
+            } else if (target.compareTo(arr[midpoint]) < 0) {
+                return search(target, Arrays.copyOfRange(arr, 0, midpoint));
+            } else if (target.compareTo(arr[midpoint]) > 0) {
+                return search(target, Arrays.copyOfRange(arr, midpoint, arr.length));
+            }
+            return false;
+        }
 }
--- src/main/java/net/approachcircle/demo/Main.java
+++ src/main/java/net/approachcircle/demo/Main.java
@@ -1,2 +1,13 @@
-package net.approachcircle.demo;public class Main {
+package net.approachcircle.demo;
+
+public class Main {
+    private static final String[] pets = {"ant", "bat", "cat", "dog", "elk", "fox", "rat"};
+    public static void main(String[] args) {
+        String target = "ant";
+        if (BinarySearch.search(target, pets)) {
+            System.out.printf("target '%s' found%n", target);
+        } else {
+            System.out.println("target not found");
+        }
+    }
 }
